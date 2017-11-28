150eb47c6d36df767234f6d5b6bb092a819b0650
Joseph Medley <jmedley@google.com>
2017-11-29 02:58:07 +0530

    Tinker with question manager.

--- questionmanager.js
+++ questionmanager.js
@@ -9,49 +9,70 @@ const reToken = /\${(?:data\.)[^}]+}!/g;
 const catalog = new questioncatalog.Catalog();
 
 function Manager(forMembers) {
-  this._qustionSets = [];
-  this._shared = new questionset.QuestionSet();
+  // this._qustionSets = [];
+  this._questions = new questionset.QuestionSet();
   this._getQuestions(forMembers);
 }
 
 Manager.prototype.constructor = Manager;
 
 Manager.prototype._getQuestions = function(forMembers) {
+  const FLAGS = ['o','i','c','e','h','m','p'];
+  const EXPANSIONS = ['overview','interface','constructor','event',
+                      'handler','method','property'];
   const members = forMembers.split('-');
   // Assumes '' is always the first one. Dangerous.
   if (members[0] == '') { members.shift(); }
-  if (!members[0].charAt(0) == 'i') { throw "The first flag must be '-i'."; }
-  const interfaceName = (members[0].split(' '))[1];
-  members.forEach((type) => {
-    let types;
-    switch (type.charAt(0)) {
-      case 'i':
-        this._qustionSets.push(this._getQuestionSet(interfaceName, "interface", this._shared));
-        break;
-      case 'c':
-        let constructorName = interfaceName + "()";
-        this._qustionSets.push(this._getQuestionSet(constructorName, "constructor", this._shared));
-        break;
-      case 'e':
-        types = type.slice(1).split(' ');
-        types.forEach((type)=> {
-          this._qustionSets.push(this._getQuestionSet(type, "event", this._shared));
-        })
-        break;
-      case 'm':
-        types = type.slice(1).split(' ');
-        types.forEach((type)=> {
-          this._qustionSets.push(this._getQuestionSet(type, "method", this._shared));
-        })
-        break;
-      case 'p':
-        types = type.slice(1).split(' ');
-        types.forEach((type)=> {
-          this._qustionSets.push(this._getQuestionSet(type, "property", this._shared));
-        })
-        break;
-    }
+  let interfaceName;
+  members.forEach((member, i) => {
+    let first = member.charAt(0);
+    // If first not in FLAGS, throw.
+    if (first=='i') { interfaceName = (member.split(' '))[1]; }
+    this._getFromTemplate(EXPANSIONS[i]);
   });
+
+
+  // const members = forMembers.split('-');
+  // // Assumes '' is always the first one. Dangerous.
+  // if (members[0] == '') { members.shift(); }
+  // if (!members[0].charAt(0) == 'i') { throw "The first flag must be '-i'."; }
+  // const interfaceName = (members[0].split(' '))[1];
+  // members.forEach((type) => {
+  //   let types;
+  //   switch (type.charAt(0)) {
+  //     case 'o':
+  //       this._questionSets.push(this._getQuestionSet(interfaceName, "overview", this._shared));
+  //     case 'i':
+  //       this._qustionSets.push(this._getQuestionSet(interfaceName, "interface", this._shared));
+  //       break;
+  //     case 'c':
+  //       let constructorName = interfaceName + "()";
+  //       this._qustionSets.push(this._getQuestionSet(constructorName, "constructor", this._shared));
+  //       break;
+  //     case 'e':
+  //       types = type.slice(1).split(' ');
+  //       types.forEach((type)=> {
+  //         this._qustionSets.push(this._getQuestionSet(type, "event", this._shared));
+  //       })
+  //       break;
+  //     case 'm':
+  //       types = type.slice(1).split(' ');
+  //       types.forEach((type)=> {
+  //         this._qustionSets.push(this._getQuestionSet(type, "method", this._shared));
+  //       })
+  //       break;
+  //     case 'p':
+  //       types = type.slice(1).split(' ');
+  //       types.forEach((type)=> {
+  //         this._qustionSets.push(this._getQuestionSet(type, "property", this._shared));
+  //       })
+  //       break;
+  //   }
+  // });
+}
+
+Manager.prototype._getFromTemplate = function(templateName) {
+
 }
 
 Manager.prototype._getQuestionSet = function(forMember, templateName, shared) {
